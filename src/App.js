8c5efce88a347e0c39ecc99a4735f8a0bfe37bce
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-19 17:12:20 -0300

    Adiciona requisito 2✔ Cria somente a rota já que o componnente já está criado com nome e localização dos diretórios corretos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={Login} /> {/*cria rota para página de login */}
+        <Route path="/carteira" component={Wallet} /> {/* rota para página de wallet */}
       </Switch>
     ) // retante no index do src para que ao renderizar o app nos teste poder manipular rotas e store separadamente;
   }
