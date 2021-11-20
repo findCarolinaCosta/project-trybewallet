385f0e440a302071fed489176a86b5cca2d7d760
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-20 19:45:01 -0300

    Adiciono na action getExpenses a resposta da api de cotações

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import exchangeApiFetch from '../services/exchangeApiFetch';
+
 export const LOGIN = 'LOGIN'; // para padronizar cria constante do tipo e exporta para também ser usado no reducer do user
 export const GET_EXPENSES = 'GET_EXPENSES';
 
@@ -5,17 +7,15 @@ export const GET_EXPENSES = 'GET_EXPENSES';
 e o dito cujo email via action para o reducer do user para ser de fato salvo no store */
 export const saveEmailLogin = (email) => ({ type: LOGIN, email });
 
-export const getExpenses = (arrayExpenses) => ({
-  type: GET_EXPENSES, arrayExpenses });
+export const getExpenses = (expenses, currentExchange) => ({
+  type: GET_EXPENSES, expenses, currentExchange });
 
-export function fetchCurrencyQuotes(arrayExpenses) {
+export function fetchCurrencyQuotes(expenses) {
   return async (dispatch) => {
     try {
-      const response = await fetch('');
-      const currency = await response.json();
-      console.log(currency, 'curr');
+      const response = await exchangeApiFetch();
+      dispatch(getExpenses(expenses, response));
     } catch (e) {
-      dispatch(getExpenses(arrayExpenses));
       console.log(e);
     }
   };
