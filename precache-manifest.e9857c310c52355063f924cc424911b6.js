e6839405d5041f86c282704bdc85fff1c178d88c
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-12-02 23:49:11 -0300

    Updates

---
+++ precache-manifest.e9857c310c52355063f924cc424911b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d04283b9c0741f70ed4ac6c1fbca75",
+    "url": "/project-trybewallet/index.html"
+  },
+  {
+    "revision": "9da9ee28cd1c45e94071",
+    "url": "/project-trybewallet/static/css/main.6078686a.chunk.css"
+  },
+  {
+    "revision": "390754d6397488c01685",
+    "url": "/project-trybewallet/static/js/2.bbea6d94.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-trybewallet/static/js/2.bbea6d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9da9ee28cd1c45e94071",
+    "url": "/project-trybewallet/static/js/main.9e878205.chunk.js"
+  },
+  {
+    "revision": "f7e9fac6e1746606b827",
+    "url": "/project-trybewallet/static/js/runtime-main.29301b93.js"
+  }
+]);
